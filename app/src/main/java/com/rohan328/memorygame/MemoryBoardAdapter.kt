6f01af343c0c9992bd49609107a064aa7a87e023
Rohan <35950830+rohan328@users.noreply.github.com>
2020-12-19 00:16:15 +0530

    imageViews added in recycler view

--- app/src/main/java/com/rohan328/memorygame/MemoryBoardAdapter.kt
+++ app/src/main/java/com/rohan328/memorygame/MemoryBoardAdapter.kt
@@ -1,26 +1,51 @@
 package com.rohan328.memorygame
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.min
 
 class MemoryBoardAdapter(private val context: Context, private val numPieces: Int) :
     RecyclerView.Adapter<MemoryBoardAdapter.ViewHolder>() {
 
+    companion object {
+        private const val MARGIN_SIZE = 10
+        private const val TAG = "MemoryBoardAdapter"
+    }
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        LayoutInflater.from(context).inflate(R.layout.memory_card, parent, false)
+        val cardWidth = parent.width / 2 - (2 * MARGIN_SIZE)
+        val cardHeight = parent.height / 4 - (2 * MARGIN_SIZE)
+        val cardSideLen = min(cardHeight, cardWidth)
+        val view = LayoutInflater.from(context).inflate(R.layout.memory_card, parent, false)
+        val layoutParams =
+            view.findViewById<CardView>(R.id.cardView).layoutParams as ViewGroup.MarginLayoutParams
+        layoutParams.width = cardSideLen
+        layoutParams.height = cardSideLen
+        layoutParams.setMargins(MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE, MARGIN_SIZE)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(position)
     }
 
     override fun getItemCount() = numPieces
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val imageButton = itemView.findViewById<ImageButton>(R.id.imageButton)
+
+        fun bind(position: Int) {
+            imageButton.setOnClickListener {
+                Log.d(TAG, "Clicked in position $position")
+            }
+        }
 
     }
 }
