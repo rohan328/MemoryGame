9a726b93a68d09c2293c05022cfb7f15edb89f74
Rohan <35950830+rohan328@users.noreply.github.com>
2020-12-19 06:18:08 +0530

    change game modes and reset game

--- app/src/main/java/com/rohan328/memorygame/MainActivity.kt
+++ app/src/main/java/com/rohan328/memorygame/MainActivity.kt
@@ -4,11 +4,18 @@ import android.animation.ArgbEvaluator
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.RadioGroup
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener
 import com.google.android.material.snackbar.Snackbar
 import com.rohan328.memorygame.models.BoardSize
 import com.rohan328.memorygame.models.MemoryGame
@@ -34,6 +41,29 @@ class MainActivity : AppCompatActivity() {
         tvNumPairs = findViewById(R.id.tvNumPairs)
         layoutRoot = findViewById(R.id.layoutRoot)
 
+        setupBoard()
+    }
+
+    //setup the game
+    @SuppressLint("SetTextI18n")
+    private fun setupBoard() {
+        //set initial textview titles
+        when (boardSize) {
+            BoardSize.EASY -> {
+                tvNumMoves.text = "EASY: 4 x 2"
+                tvNumPairs.text = "Pairs: 0/${boardSize.getNumPairs()}"
+            }
+            BoardSize.MEDIUM -> {
+                tvNumMoves.text = "MEDIUM: 6 x 3"
+                tvNumPairs.text = "Pairs: 0/${boardSize.getNumPairs()}"
+            }
+            BoardSize.HARD -> {
+                tvNumMoves.text = "HARD: 6 x 6"
+                tvNumPairs.text = "Pairs: 0/${boardSize.getNumPairs()}"
+            }
+
+        }
+
         //set zero progress
         tvNumPairs.setTextColor(ContextCompat.getColor(this, R.color.color_progress_none))
 
@@ -57,6 +87,7 @@ class MainActivity : AppCompatActivity() {
         rvBoard.layoutManager = GridLayoutManager(this, boardSize.getWidth())
     }
 
+    //update game(flip images)
     @SuppressLint("SetTextI18n")
     private fun updateGame(position: Int) {
         //if already won show message and return
@@ -97,4 +128,72 @@ class MainActivity : AppCompatActivity() {
         tvNumMoves.text = "Moves: ${memoryGame.getNumMoves()}"
         adapter.notifyDataSetChanged() //notify adapter
     }
+
+    //inflate menu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    //menu item click listener
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.miRefresh -> {
+                //reset game
+                if (memoryGame.getNumMoves() > 0 && !memoryGame.haveWonGame()) {
+                    showAlertDialog("Quit your current game?", null) {
+                        setupBoard()
+                    }
+                } else {
+                    setupBoard()
+                }
+                return true
+            }
+            R.id.miNewSize -> {
+                showNewSizeDialog()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    @SuppressLint("InflateParams")
+    private fun showNewSizeDialog() {
+        //inflate the radiobutton view
+        val boardSizeView = LayoutInflater.from(this).inflate(R.layout.dialog_board_size, null)
+        val radioGroupSize = boardSizeView.findViewById<RadioGroup>(R.id.radioGroup)
+
+        //set the radiobutton to selected game mode
+        when (boardSize) {
+            BoardSize.EASY -> radioGroupSize.check(R.id.rbEasy)
+            BoardSize.MEDIUM -> radioGroupSize.check(R.id.rbMedium)
+            BoardSize.HARD -> radioGroupSize.check(R.id.rbHard)
+        }
+
+        //show dialog and reload game accordingly
+        showAlertDialog("Choose New Size", boardSizeView) {
+            boardSize = when (radioGroupSize.checkedRadioButtonId) {
+                R.id.rbEasy -> BoardSize.EASY
+                R.id.rbMedium -> BoardSize.MEDIUM
+                else -> BoardSize.HARD
+            }
+            setupBoard()
+        }
+    }
+
+    //show alert dialog
+    private fun showAlertDialog(
+        title: String,
+        view: View?,
+        positiveButtonClickListener: View.OnClickListener
+    ) {
+
+        AlertDialog.Builder(this)
+            .setTitle(title).setView(view)
+            .setNegativeButton("Cancel", null)
+            .setPositiveButton("Okay") { _, _ ->
+                positiveButtonClickListener.onClick(null)
+            }.show()
+    }
+
 }
